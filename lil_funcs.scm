3c84f1a012d8a659bd17586cbb88da45f56de3fd
Jason <bjason@gmail.com>
2014-10-16 01:44:49 -0700

    List messings

--- lil_funcs.scm
+++ lil_funcs.scm
@@ -1,5 +1,25 @@
+(define x 4)
+(display x)
+(newline)
+; factorIal functIon
+
+(define fact
+  (lambda (n)
+    (if (= n 0)
+      1
+      (* n (fact (- n 1))))))
+(display (fact 3))
+(newline)
+(define (addFive x)
+  (+ 5 x))
+(addFive 10)
 (define (makelist n)
   (if (= n 0)
     (list 0)
     (cons n (makelist (- n 1)))))
 (makelist 4)
+(define (add5list items)
+  (if (null? items)
+    '()
+    (cons (addFive (car items)) (add5list (cdr items)))))
+(add5list '(1 2 3 4 5))
